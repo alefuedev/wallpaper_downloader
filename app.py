3d4ce47c8fda8b01fd4db5fc4e34b78e966e6c89
Ale <alefuentesdev@gmail.com>
2021-01-11 05:04:53 -0500

    Check if category exist if so , download random image

--- app.py
+++ app.py
@@ -1,24 +1,35 @@
+import random
 import requests
 import uuid
 from bs4 import BeautifulSoup
 
+while True:
+    category = input("Which category are you interested: ")
+    url = f"https://wallhaven.cc/search?q={category}"
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, "html.parser")
+    notice = str(soup.find("p", "pagination-notice")).rsplit(" ")
+    category_found = True
 
-category = input("Which category are you interested: ")
-url = f"https://wallhaven.cc/search?q={category}"
-r = requests.get(url)
+    for word in notice:
+        if "nothing" == word:
+            category_found = False
+            print("Category not found.")
 
-soup = BeautifulSoup(r.content, "html.parser")
-links = soup.find_all("a", "preview")
-first_link = links[0]
-wallpaper_location = first_link["href"]
+    if category_found == True:
+        links = soup.find_all("a", "preview")
+        links_length = len(links) - 1
+        random_link = random.randint(0, links_length)
+        first_link = links[random_link]
+        wallpaper_location = first_link["href"]
 
-r2 = requests.get(wallpaper_location)
-soup2 = BeautifulSoup(r2.content, "html.parser")
-img_tag = soup2.find(id="wallpaper")
-alt = img_tag["alt"]
-src = img_tag["src"]
-random_id = str(uuid.uuid4()).rsplit("-")[-1]
+        r2 = requests.get(wallpaper_location)
+        soup2 = BeautifulSoup(r2.content, "html.parser")
+        img_tag = soup2.find(id="wallpaper")
+        alt = img_tag["alt"]
+        src = img_tag["src"]
+        random_id = str(uuid.uuid4()).rsplit("-")[-1]
 
-# Download image
-wallpaper = requests.get(src)
-open(f"{category}.jpg", "wb").write(wallpaper.content)
+        # Download image
+        wallpaper = requests.get(src)
+        open(f"{category}.jpg", "wb").write(wallpaper.content)
